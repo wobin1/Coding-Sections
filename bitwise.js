bb82a59b1fb87dad26df0dba565d270e3b8265da
Wobin <nathanielmusa3@gmail.com>
2021-05-26 12:01:06 +0100

    added a bitwise oprators coding session

---
+++ bitwise.js
@@ -0,0 +1,14 @@
+var a = 2;
+var b = 3;
+
+var result;
+result = (a & b);
+console.log("(a & b)=>", result);
+result = (a | b);
+console.log("(a | b) =>", result);
+result = (a ^ b);
+console.log("(a ^ b) =>", result)
+result = (~b);
+console.log("(~b) =>", result);
+result = (a << b);
+console.log("(a >> b) =>", result);
